8f0f862fc1f6cb8b545c801a5ab79e5af39ba178
tristan <thuckabee.work@gmail.com>
2024-07-12 03:22:00 -0500

    done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log(navigator.clipboard);
     let copyButton = document.querySelector('#copy-button');
     copyButton.addEventListener('click', () => {
         navigator.clipboard.writeText(window.location.href);
